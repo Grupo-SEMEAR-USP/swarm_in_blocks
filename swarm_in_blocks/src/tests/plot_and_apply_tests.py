aa601339a9e33ca5f7e57d2d9e5cfe526d020264
Rafael-Saud <rafaelsaudferro@gmail.com>
2022-02-18 17:08:09 -0300

    Starting test the plot and apply feature

---
+++ swarm_in_blocks/src/tests/plot_and_apply_tests.py
@@ -0,0 +1,62 @@
+from tkinter import * 
+#from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+  
+# plot function is created for plotting the graph in tkinter window
+def plot():
+
+    fig = plt.figure(figsize=(8, 8))
+    #plt.subplots_adjust(bottom = 0.2)
+    plt.plot([0,1,2,3,4,5],[0,1,2,3,4,5],'ro')
+    plt.axis([-1,11,-1,11])
+    plt.grid(True)
+    plt.xticks(np.linspace(0,10,11))
+    plt.yticks(np.linspace(0,10,11))
+    #posit = plt.axes([0.4, 0.1, 0.2, 0.05])
+    #plt.show(block=False)
+  
+    # the figure that will contain the plot
+    #fig = Figure(figsize = (5, 5))
+  
+    # list of squares
+    #y = [i**2 for i in range(101)]
+  
+    # adding the subplot
+    #plot1 = fig.add_subplot(111)
+  
+    # plotting the graph
+    #plot1.plot(y)
+  
+    # creating the Tkinter canvas containing the Matplotlib figure
+    canvas = FigureCanvasTkAgg(fig, master = window)  
+    canvas.draw()
+  
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+  
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas, window)
+    toolbar.update()
+  
+    # placing the toolbar on the Tkinter window
+    canvas.get_tk_widget().pack()
+  
+# the main Tkinter window
+window = Tk()
+  
+# setting the title 
+window.title('Formation preview')
+  
+# dimensions of the main window
+window.geometry("500x500")
+  
+# button that displays the plot
+plot_button = Button(master = window, command = plot, height = 2, width = 10, text = "Plot")
+  
+# place the button in main window
+plot_button.pack()
+  
+# run the gui
+window.mainloop()
