4a4db1a8582acd2fde1b004479c757d01fb268b7
Felipe-Tommaselli <f.tommaselli@usp.br>
2022-02-20 18:53:02 -0300

    finalizando dark mode e iniciando o modal do Launch

--- swarm_clover_blocks/www/main.js
+++ swarm_clover_blocks/www/main.js
@@ -58,6 +58,35 @@ var ready = readParams(); // initialization complete promise
 
 var pythonArea = document.getElementById('python');
 
+// modal overlay 
+
+const viewBtn = document.querySelector(".view-modal"),
+popup = document.querySelector(".popup"),
+close = popup.querySelector(".close"),
+field = popup.querySelector(".field"),
+input = field.querySelector("input"),
+copy = field.querySelector("button");
+
+viewBtn.onclick = ()=>{
+	popup.classList.toggle("show");
+}
+close.onclick = ()=>{
+	viewBtn.click();
+}
+
+copy.onclick = ()=>{
+  input.select(); //select input value
+  if(document.execCommand("copy")){ //if the selected text copy
+	field.classList.add("active");
+	copy.innerText = "Copied";
+	setTimeout(()=>{
+	  window.getSelection().removeAllRanges(); //remove selection from document
+	field.classList.remove("active");
+	copy.innerText = "Copy";
+	}, 3000);
+}
+}
+
 // update Python code
 workspace.addChangeListener(function(e) {
 	ready.then(function() {
