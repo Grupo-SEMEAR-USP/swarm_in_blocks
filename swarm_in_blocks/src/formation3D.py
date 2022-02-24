84170f55c76adf5bfb7af9334db535e2c1bcaa8a
Guilherme Soares <gsoares.silvestre@gmail.com>
2022-02-23 21:21:35 -0300

    Testing new options on formations 3D

--- swarm_in_blocks/src/formation3D.py
+++ swarm_in_blocks/src/formation3D.py
@@ -8,8 +8,7 @@ def formation3DFromMesh(model_path, number_of_points):
     if mesh.is_empty():
         raise Exception("Mesh is empty.")
     
-    mesh.compute_vertex_normals()
-    pcd = mesh.sample_points_uniformly(number_of_points=number_of_points)
+    pcd = mesh.sample_points_poisson_disk(number_of_points=number_of_points, init_factor=5)
     
     # Line vectors
     coords = np.array(pcd.points)
@@ -22,10 +21,10 @@ def formation3DFromMesh(model_path, number_of_points):
 def formation3DfromMeshZoo(mesh_name):
     pass
 
-
 def visualizeMesh(mesh):
     if mesh is None:
         raise Exception("Mesh is empty.")
+    mesh.compute_vertex_normals()
     o3d.visualization.draw_geometries([mesh])
 
 def visualizePointCloud(pcd):
@@ -34,8 +33,9 @@ def visualizePointCloud(pcd):
         raise Exception("PointCloud is empty.")
     o3d.visualization.draw_geometries([pcd])
     
-def 
-    
 if __name__ == '__main__':
-    coords, mesh, pcd = setFormationFrom3DModel('/home/guisoares/.ros/swarm_models/christ-the-redeemer/Christ the Redeemer.obj', 100)
+
+    # path = '/home/guisoares/.ros/swarm_models/christ-the-redeemer/Christ the Redeemer.obj'
+    path = '/home/guisoares/Downloads/output (1).stl'
+    coords, mesh, pcd = formation3DFromMesh(path, 100)
     visualizePointCloud(pcd)
