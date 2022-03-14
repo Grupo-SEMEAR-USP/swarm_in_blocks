6a2dfbec0231ef16d34798c4c7de9af23b627d78
Guilherme Soares <gsoares.silvestre@gmail.com>
2022-03-14 17:03:42 -0300

    Working on led

--- swarm_clover_blocks/www/launch.php
+++ swarm_clover_blocks/www/launch.php
@@ -1,6 +1,14 @@
 <?php
     // $command = escapeshellcmd('');
     error_log(print_r("TESTE",true));
-    $output = shell_exec("bash /home/guisoares/.bashrc && roslaunch swarm_in_blocks swarm_main.launch num=:1");
+    // $output = shell_exec("roslaunch swarm_in_blocks swarm_main.launch num=:1");
+    
+    // $output = exec("source");
+    putenv('PATH=/home/guisoares/catkin_ws/devel/bin:/opt/ros/noetic/bin:/home/guisoares/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin');
+    putenv('PYTHONPATH=/home/guisoares/catkin_ws/devel/lib/python3/dist-packages:/opt/ros/noetic/lib/python3/dist-packages');
+    putenv('LD_LIBRARY_PATH=/home/guisoares/catkin_ws/devel/lib:/opt/ros/noetic/lib::/usr/lib/x86_64-linux-gnu/:/home/guisoares/catkin_ws/install/lib/mavlink_sitl_gazebo/plugins');
+    $output = exec("python3 launch.py");
+    // $output = exec("echo \$HOME");
     echo $output;
+    error_log(print_r($output,true));
 ?>
