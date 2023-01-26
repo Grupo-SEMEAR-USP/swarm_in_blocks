088104d34d6c2b085f223831c02e61d0ae02059c
joseCarlosAndrade <josecarlos.101@usp.br>
2023-01-25 22:44:18 -0300

    fpv commands almostsstst finished

--- swarm_fpv/src/js/fpv_prelude/script.js
+++ swarm_fpv/src/js/fpv_prelude/script.js
@@ -8,7 +8,7 @@ function updateContent(text) {
 
     // do ROS stuff here
 }
-
+var vel = 2;
 // objetct
 function Drone(id, name) {
         // ROS parts
@@ -28,13 +28,13 @@ function Drone(id, name) {
         // };
 
         // protopipo de request
-        this.setRequest = (vx=0, vy=0, vz=0, ) => {
+        this.setRequest = (vz=0, vy=0, vx=0, yr=0) => {
             let request = new ROSLIB.ServiceRequest({
                 vx : vx,
                 vy : vy,
                 vz : vz,
                 yaw : 0,
-                yaw_rate : 0,
+                yaw_rate : yr,
                 frame_id : this.body,
                 auto_arm : true
                 // --- response:
@@ -51,45 +51,55 @@ function Drone(id, name) {
         this.KeyW =  function () {
             updateContent("up");
             // pubMsg("W FOI PRESSIONADO")
-            this.setVelocity.callService(this.setRequest(vz=5), function (result) {
+            this.setVelocity.callService(this.setRequest(vz=vel, vy=0, vx=0, yr=0), function (result) {
                 console.log(result)
             });
         };
         this.KeyA = () => {
             updateContent("yaw+");
+            this.setVelocity.callService(this.setRequest(vz=0, vy=0, vx=0, yr=vel), function (result) {
+                console.log(result)
+            });
             // pubMsg("A FOI PRESSIONADO")
         },
         this.KeyS = () => {
             updateContent("down");
+            this.setVelocity.callService(this.setRequest(vz=-vel, vy=0, vx=0, yr=0), function (result) {
+                console.log(result)
+            });
             // pubMsg("S FOI PRESSIONADO")
         },
         this.KeyD = () => {
             updateContent("yaw-");
+            this.setVelocity.callService(this.setRequest(vz=0, vy=0, vx=0, yr=-vel), function (result) {
+                console.log(result)
+            });
             // pubMsg("D FOI PRESSIONADO")
         },
 
+        
         // right joystick
         this.ArrowUp = () => {
             updateContent("foward")
-            this.setVelocity.callService(this.setRequest(vx=2), function (result) {
+            this.setVelocity.callService(this.setRequest(vz=0, vy=0, vx=vel, yr=0), function (result) {
                 console.log(result)
             });
         },
         this.ArrowDown = () => {
             updateContent("backward")
-            this.setVelocity.callService(this.setRequest(vx=-2), function (result) {
+            this.setVelocity.callService(this.setRequest(vz=0, vy=0, vx=-vel, yr=0), function (result) {
                 console.log(result)
             });
         },
         this.ArrowLeft = () => {
             updateContent("left")
-            this.setVelocity.callService(this.setRequest(vy=-2), function (result) {
+            this.setVelocity.callService(this.setRequest(vz=0, vy=vel, vx=0, yr=0), function (result) {
                 console.log(result)
             });
         },
         this.ArrowRight = () => {
             updateContent("right")
-            this.setVelocity.callService(this.setRequest(vy=2), function (result) {
+            this.setVelocity.callService(this.setRequest(vz=0, vy=-vel, vx=0, yr=0), function (result) {
                 console.log(result)
             });
         },
