ab2eb027a31ca46a0363c03925eead93d01fea1a
uspnathan <nathvilas8@gmail.com>
2023-01-30 11:08:15 -0300

    Código fpv_clover

---
+++ swarm_fpv/src/js/fpv_script.js
@@ -0,0 +1,101 @@
+var ros = new ROSLIB.Ros({
+    url: 'ws://localhost:9099'
+  });
+  ros.on('connection', function() {
+    console.log('Connected to websocket server.');
+  });
+  ros.on('error', function(error) {
+    console.log('Error connecting to websocket server: ', error);
+  });
+  ros.on('close', function() {
+    console.log('Connection to websocket server closed.');
+  });
+  // ----------------------
+  // var lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  var list = [];
+  
+  function define_id(id_c) {
+    id = id_c;
+  }
+  
+  function reload_subscriber(elemento) {
+    update_listener(elemento.value);
+  }
+  
+  function aumentaImagem(nome) {
+    nome.width = nome.width + 850;
+    nome.height = nome.height + 850;
+  }
+  function tamanhoNormal(nome) {
+    nome.width = 850;
+    nome.height = 850;
+  }
+  
+  
+  var listener;
+  function update_listener(id) {
+    if(id == null){
+        return
+    }
+    if (listener) {
+      listener.unsubscribe();
+    }
+  
+    listener = new ROSLIB.Topic({
+      ros: ros,
+      name: `clover${id}/main_camera/image_raw/compressed`,
+      messageType: 'sensor_msgs/CompressedImage'
+    });
+    listener.subscribe(function(message) {
+      console.log('Received message on ' + listener.name);
+      document.getElementById('image_sub').src = "data:image/jpeg;base64," + message.data;
+    });
+  }
+  
+  //update_listener(0);
+  
+  // atualizaçao de lista de ids corretas
+  var listenerSwarm;
+  
+  listenerSwarm = new ROSLIB.Topic({
+    ros: ros,
+    name: `swarm_checker/state`,
+    messageType: 'swarm_checker/SwarmState'
+  
+  });
+  
+  listenerSwarm.subscribe(function(message) {
+    // document.getElementById('connected_ids')
+    list = message.all_clovers_ids // [0, 1, 3, 5] TO DO -> Na verdade é with_camera_ids, mas para simulaçao 
+    listenerSwarm.unsubscribe()
+    addIds()
+  });
+  
+  function addIds() {
+    var element = document.getElementById('dropdown')
+  
+    for (let id in list) {
+      // create_element(lista[x])
+      var child = document.createElement('option')
+      console.log(id)
+      child.textContent = `Clover ${list[id]}`
+      child.setAttribute('value', `${list[id]}`)
+      element.appendChild(child)
+    }
+  }
+  
+ 
+  
+  // function create_element(id) {
+  //   var element = document.getElementById('dropdown')
+  //   var child = document.createElement('option')
+  //   child.textContent = `Clover ${id}`
+  //   child.setAttribute('value', `${id}`)
+  //   element.appendChild(child)
+  // }
+  
+  // cameraId = new ROSLIB.Topic({
+  //   ros: ros,
+  //   name: 'swarm_checker/state'`
+  // })
+  
