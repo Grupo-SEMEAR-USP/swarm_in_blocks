42c938e12fad39e19165b79bdad097b53bfee1aa
AgnesBressan <agnesbressan3838@gmail.com>
2023-02-26 22:08:34 -0300

    emergency land button

--- swarm_station/3d.js
+++ swarm_station/3d.js
@@ -146,3 +146,16 @@ setScene('map')
 addAxes()
 addVehicle()
 
+const openModalButton = document.querySelector(".open-modal");
+const closeModalButton = document.querySelector(".close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+	modal.classList.toggle("hide");
+	fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+	el.addEventListener("click", () => toggleModal());
+});
