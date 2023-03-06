e9a2111d4833973afb73a614d3de6cd8d03f6469
MatheusDrm <matheus.drocca@usp.br>
2023-03-05 21:12:00 -0300

    Update on data read from raspberry

--- swarm_clover_blocks/www/web_terminal/index.js
+++ swarm_clover_blocks/www/web_terminal/index.js
@@ -2,7 +2,7 @@
 
 var terminalCounter = 0;
 document.getElementById("open-terminal-button").addEventListener("click", function() {
-    console.log("Creating new terminal")
+    console.log("Creating new tasasaserminal")
     // setTimeout(function() {
     //     console.log("Esta mensagem será exibida após 1 segundo.");
     //   }, 1000);
@@ -11,6 +11,8 @@ document.getElementById("open-terminal-button").addEventListener("click", functi
     terminalDiv.className = webTerminal;
     terminalDiv.id = terminalCounter - 1;
     document.body.appendChild(terminalDiv);
+    terminalDiv.style.marginBottom = "5px"; // adiciona um estilo CSS para ajustar a margem inferior
+    // document.body.appendChild(document.createElement("br"));
     // var webTerminal = new webTerminal(document.getElementById("terminal" + terminalCounter));
     console.log("Terminal div: ", terminalDiv);
     let terminal = new webTerminal(terminalDiv.id);
